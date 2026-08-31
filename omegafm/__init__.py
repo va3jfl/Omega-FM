39febe3acb88d00734cd9e6b7a820eb03d0d84dc
Joel Lagace <87234018+va3jfl@users.noreply.github.com>
2026-08-31 09:12:16 -0400

    Delete omegafm directory

--- omegafm/__init__.py
+++
@@ -1,3 +0,0 @@
-"""OmegaFM - digital FM broadcast audio processor."""
-
-__version__ = "1.20.1"
--- omegafm/dsp/__init__.py
+++
@@ -1,3 +0,0 @@
-"""OmegaFM DSP toolbox - filters, dynamics, resampling."""
-
-from . import filters, dynamics, resample  # noqa: F401
